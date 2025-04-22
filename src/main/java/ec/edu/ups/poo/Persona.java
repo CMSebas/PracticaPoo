8d2f63c83edcc9bf62535590b0e24fcb658b44c8
CMSebas <rafaselm918@gmail.com>
2025-04-21 23:34:20 -0500

    Primera Version

---
+++ src/main/java/ec/edu/ups/poo/Persona.java
@@ -0,0 +1,56 @@
+package ec.edu.ups.poo;
+public class Persona {
+    // Atributos
+
+    private String nombre;
+    private String apellido;
+    private String cedula;
+
+    //* Constructores *
+    // tiene que ser publicos || no regresa nada || tiene que tener el mismo nombre que la clase
+    // Sirve para instanciar
+
+    public Persona() {
+        // Constructor vacío
+    }
+
+    public Persona(String nombre, String apellido, String cedula, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+    }
+
+    // Getter and setter
+    // Asignar datos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Cedula: " + cedula + "\nNombre: " + nombre + "\nApellido: " + apellido + "\nDireccion: " ;
+    }
+}
